912c2a602daf5c8092b9cd458bc2116540fc1ebc
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-24 09:55:23 +0530

    REFACTORED: UC2 Win or Lose

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -3,9 +3,25 @@ public class GamblerSimulator {
 	
 	public static final double INITIAL_STAKE = 100;
 	public static final double STAKE_BET = 100;
+	public static int stake = 0;
+	
+	public static void winOrLose() {
+	
+		if(Math.random()<0.5) {
+			stake++;
+			System.out.println("Win");;
+		}
+		else {
+			stake --;
+			System.out.println("Lose");
+			
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambler Simulation");
+		
 	
 	}
 
