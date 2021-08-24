47646f76ae2754e308000c050a06a0d9fe8f1928
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-24 10:37:18 +0530

    REFACTORED: UC3 Gambler Resignes

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,28 +1,41 @@
 package com.bridgelabz.gamblersimulator;
+
 public class GamblerSimulator {
-	
+
 	public static final double INITIAL_STAKE = 100;
-	public static final double STAKE_BET = 100;
+	public static final double STAKE_BET = 1;
 	public static int stake = 0;
 	
-	public static void winOrLose() {
-	
-		if(Math.random()<0.5) {
-			stake++;
-			System.out.println("Win");;
+
+	public static boolean winOrLose() {
+
+		if ((int)Math.floor(Math.random()*10)%2 == 1) {
+			return true;
+		} else {
+			return false;
 		}
-		else {
-			stake --;
-			System.out.println("Lose");
+	}
+
+	public static void gamblerDecision() {
+		boolean resign = false;
+		
+		while(resign == false) {
+			
+			if (winOrLose()) {
+				stake ++;
+			} else {
+				stake --;
+			}
+			if (stake >= INITIAL_STAKE + (INITIAL_STAKE * 0.5) || stake <= INITIAL_STAKE / 2) {
+				resign = true;
+			} 
 			
 		}
 	}
-	
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambler Simulation");
-		
-	
+		gamblerDecision();
 	}
 
 }
