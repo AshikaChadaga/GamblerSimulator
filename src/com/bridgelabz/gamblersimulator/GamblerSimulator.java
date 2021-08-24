dcadc7c103036d0d4bdd05ccdc63ec8a07d74b41
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-24 09:29:04 +0530

    Welcome Message

---
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gamblersimulator;
+public class GamblerSimulator {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welocme To Gambler Simulation");
+		
+		
+		
+	}
+
+}
